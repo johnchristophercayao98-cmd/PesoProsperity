49a01e1d39a0551db68ece93905cccedb91a0728
John Christopher Cayao <johnchristophercayao98@gmail.com>
2025-11-30 07:54:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9555302336-10acc",
+  "appId": "1:160525147118:web:49c549ea7e07386f564cff",
+  "apiKey": "AIzaSyCDQQVLyXLDsHmWWLF6FJICLa8_T9FRLVY",
+  "authDomain": "studio-9555302336-10acc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "160525147118"
+};
