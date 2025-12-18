e1078e3059e7ad6af4c9a762cea40b2c10448749
John Christopher Cayao <johnchristophercayao98@gmail.com>
2025-12-18 23:03:06 +0000

    can you fix the save photo in change profile picture because if i click

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9555302336-10acc",
   "appId": "1:160525147118:web:49c549ea7e07386f564cff",
+  "storageBucket": "studio-9555302336-10acc.appspot.com",
   "apiKey": "AIzaSyCDQQVLyXLDsHmWWLF6FJICLa8_T9FRLVY",
   "authDomain": "studio-9555302336-10acc.firebaseapp.com",
   "measurementId": "",
